54959a6c6f70d53554030845858bdc0653557e30
Varad163 <varadgholap163@gmail.com>
2026-07-06 20:31:26 +0530

    feat(react): add Next.js App Router integration template and example app

---
+++ submissions/react/nextjs-integration-template-em/.next/static/chunks/app/layout-80c27f492edb4ac6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5023:function(n,e,t){Promise.resolve().then(t.t.bind(t,7960,23)),Promise.resolve().then(t.t.bind(t,428,23))},428:function(){},7960:function(){}},function(n){n.O(0,[452,430,803,499,971,117,744],function(){return n(n.s=5023)}),_N_E=n.O()}]);
